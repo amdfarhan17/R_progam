3e4e870abb42cbda16830b271204d254f1290662
Farhan <farhanpvt2005@gmail.com>
2025-12-16 11:21:07 +0530

    Refactor temperature conversion functions in R
    Updated temperature conversion functions and menu display.

--- 1_lab.R
+++ 1_lab.R
@@ -1,24 +1,24 @@
-# Function to convert Celsius to Fahrenheit
+
 cel_to_fah <- function(cel)
 {
   return((cel * 9/5) + 32)
 }
 
-# Function to convert Fahrenheit to Celsius
+
 fah_to_cel <- function(fah)
 {
   return((fah - 32) * 5/9)
 }
 
-# Display menu
+
 cat("Temperature Converter\n")
 cat("1. Fahrenheit to Celsius\n")
 cat("2. Celsius to Fahrenheit\n")
 
-# Read user choice
+
 choice <- as.integer(readline(prompt = "Enter your choice (1 or 2): "))
 
-# Perform conversion
+
 if (choice == 1)
 {
   fah <- as.numeric(readline(prompt = "Enter temperature in Fahrenheit: "))
